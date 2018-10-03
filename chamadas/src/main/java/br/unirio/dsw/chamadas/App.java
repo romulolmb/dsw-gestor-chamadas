c9200cd543dd4cf719752f82efdad97cb771a315
romulolmb <rombrito.luiz@gmail.com>
2018-10-03 20:42:37 -0300

    Cria projeto

---
+++ chamadas/src/main/java/br/unirio/dsw/chamadas/App.java
@@ -0,0 +1,13 @@
+package br.unirio.dsw.chamadas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
